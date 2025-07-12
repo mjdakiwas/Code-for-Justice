aa145dea6b5fefba2a7e2ea697a32e637f3300ed
Marlette Dakiwas <96099971+mjdakiwas@users.noreply.github.com>
2025-07-12 11:00:24 -0400

    updated contacts and index page

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 $(document).ready(function () {
-    M.AutoInit();
     $('select').formSelect();
     $('.tabs').tabs();
     $('.chips').chips();
+    $('.chips-placeholder').chips({
+        placeholder: 'Enter a skill',
+        secondaryPlaceholder: '+Skill',
+    });
+    $('.chips-autocomplete').chips({
+        autocompleteOptions: {
+            data: {
+                'HTML5': null,
+                'CSS3': null,
+                'JavaScript': null,
+                'WAVE': null,
+                'Siteimprove': null,
+                'ARIA': null,
+                'NVDA': null
+            },
+            limit: Infinity,
+            minLength: 1
+        }
+    });
+    $('.datepicker').datepicker({
+        format: 'mmm yyyy'
+    });
+    $('.modal').modal();
+    $('.parallax').parallax();
 })
 
 let contact_select = document.querySelector('#contact-select');
@@ -30,5 +53,19 @@ let contact_select = document.querySelector('#contact-select');
     }
 })).trigger('change');
 
+
+
+($('#website-type').on('change', function () {
+    let website_type = $('#website-type').find('option:selected').val();
+    if (website_type == 'business') {
+        $('#business').show();
+    } else {
+        $('#business').hide();
+    }
+}))
+
 // js form handling
 // validation
+$('#bank').on('click', function () {
+    $('#404').show();
+})
