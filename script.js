8ba64e8bb22cd857d1ebebd0f9d907e8792e3c8c
Marlette Dakiwas <96099971+mjdakiwas@users.noreply.github.com>
2025-07-11 20:20:54 -0400

    updated form + js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    M.AutoInit();
     $('select').formSelect();
     $('.tabs').tabs();
+    $('.chips').chips();
 })
 
 let contact_select = document.querySelector('#contact-select');
@@ -8,19 +10,19 @@ let contact_select = document.querySelector('#contact-select');
     console.log('contact type changed')
     let selected_option = $(contact_select).find('option:selected').val();
     console.log(`selected: ${selected_option}`);
-    
+
     if (selected_option == 'donate') {
         $('#donate').show();
     } else {
         $('#donate').hide();
     }
-    
+
     if (selected_option == 'volunteer') {
         $('#volunteer').show();
     } else {
         $('#volunteer').hide();
     }
-    
+
     if (selected_option == 'request') {
         $('#request').show();
     } else {
